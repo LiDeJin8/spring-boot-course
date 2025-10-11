3280e5a66af5e00bf87838dc4b39646d53903f5d
LiDeJin <381249925@qq.com>
2025-10-11 13:15:45 +0800

    feat: 完成定时任务

--- spring-boot-schedule/src/main/java/top/ldj/springbootschedule/ScheduledApplication.java
+++ spring-boot-schedule/src/main/java/top/ldj/springbootschedule/ScheduledApplication.java
@@ -1,9 +1,20 @@
 package top.ldj.springbootschedule;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
 /**
  * @Author: lenovo
  * @Date: 2025/10/10
  * @Version: 1.0
  */
+// 关键注解：开启定时任务支持
+@EnableScheduling
+@SpringBootApplication
 public class ScheduledApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScheduledApplication.class, args);
+    }
 }
---
+++ spring-boot-schedule/src/main/java/top/ldj/springbootschedule/mapper/UserAccountMapper.java
@@ -0,0 +1,15 @@
+package top.ldj.springbootschedule.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+import top.ldj.springbootschedule.entity.UserAccount;
+
+/**
+ * @Author: lenovo
+ * @Date: 2025/10/11
+ * @Version: 1.0
+ */
+@Mapper
+public interface UserAccountMapper extends BaseMapper<UserAccount> {
+
+}
